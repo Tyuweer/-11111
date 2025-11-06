9848472a949ee02efcb21a4b56c993fcb0df85e8
ALEKS <Tyuweer01@gmail.com>
2025-11-06 17:38:37 +0700

    Добавлена сортировка и всякие штуки в WinForm

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -10,7 +10,6 @@
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnGroup;
-        private System.Windows.Forms.ListBox listBoxBooks;
 
         protected override void Dispose(bool disposing)
         {
@@ -20,15 +19,29 @@
 
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.txtTitle = new System.Windows.Forms.TextBox();
             this.txtAuthor = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnGroup = new System.Windows.Forms.Button();
-            this.listBoxBooks = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            this.contextMenuSortFilter = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sortAToZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortZToAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.removeFilterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBoxSort = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Author = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
+            this.contextMenuSortFilter.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtTitle
@@ -52,7 +65,7 @@
             this.btnAdd.Size = new System.Drawing.Size(80, 23);
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "Добавить";
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // btnDelete
             // 
@@ -61,7 +74,7 @@
             this.btnDelete.Size = new System.Drawing.Size(80, 23);
             this.btnDelete.TabIndex = 3;
             this.btnDelete.Text = "Удалить";
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
             // 
             // btnUpdate
             // 
@@ -70,7 +83,7 @@
             this.btnUpdate.Size = new System.Drawing.Size(80, 23);
             this.btnUpdate.TabIndex = 4;
             this.btnUpdate.Text = "Обновить";
-            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
             // 
             // btnGroup
             // 
@@ -79,15 +92,7 @@
             this.btnGroup.Size = new System.Drawing.Size(107, 23);
             this.btnGroup.TabIndex = 5;
             this.btnGroup.Text = "Группировка";
-            this.btnGroup.Click += new System.EventHandler(this.btnGroup_Click);
-            // 
-            // listBoxBooks
-            // 
-            this.listBoxBooks.ItemHeight = 16;
-            this.listBoxBooks.Location = new System.Drawing.Point(12, 117);
-            this.listBoxBooks.Name = "listBoxBooks";
-            this.listBoxBooks.Size = new System.Drawing.Size(736, 292);
-            this.listBoxBooks.TabIndex = 6;
+            this.btnGroup.Click += new System.EventHandler(this.BtnGroup_Click);
             // 
             // label1
             // 
@@ -107,9 +112,120 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Автор книги";
             // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Title,
+            this.Author});
+            this.dataGridViewBooks.ContextMenuStrip = this.contextMenuSortFilter;
+            this.dataGridViewBooks.Location = new System.Drawing.Point(12, 71);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.RowHeadersWidth = 51;
+            this.dataGridViewBooks.RowTemplate.Height = 24;
+            this.dataGridViewBooks.Size = new System.Drawing.Size(949, 396);
+            this.dataGridViewBooks.TabIndex = 9;
+            // 
+            // contextMenuSortFilter
+            // 
+            this.contextMenuSortFilter.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuSortFilter.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sortAToZToolStripMenuItem,
+            this.sortZToAToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.removeFilterToolStripMenuItem,
+            this.selectAllToolStripMenuItem});
+            this.contextMenuSortFilter.Name = "contextMenuSortFilter";
+            this.contextMenuSortFilter.Size = new System.Drawing.Size(229, 106);
+            // 
+            // sortAToZToolStripMenuItem
+            // 
+            this.sortAToZToolStripMenuItem.Name = "sortAToZToolStripMenuItem";
+            this.sortAToZToolStripMenuItem.Size = new System.Drawing.Size(228, 24);
+            this.sortAToZToolStripMenuItem.Text = "Сортировка от А до Я";
+            this.sortAToZToolStripMenuItem.Click += new System.EventHandler(this.SortAToZToolStripMenuItem_Click);
+            // 
+            // sortZToAToolStripMenuItem
+            // 
+            this.sortZToAToolStripMenuItem.Name = "sortZToAToolStripMenuItem";
+            this.sortZToAToolStripMenuItem.Size = new System.Drawing.Size(228, 24);
+            this.sortZToAToolStripMenuItem.Text = "Сортировка от Я до А";
+            this.sortZToAToolStripMenuItem.Click += new System.EventHandler(this.SortZToAToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(225, 6);
+            // 
+            // removeFilterToolStripMenuItem
+            // 
+            this.removeFilterToolStripMenuItem.Name = "removeFilterToolStripMenuItem";
+            this.removeFilterToolStripMenuItem.Size = new System.Drawing.Size(228, 24);
+            this.removeFilterToolStripMenuItem.Text = "Удалить фильтр";
+            this.removeFilterToolStripMenuItem.Click += new System.EventHandler(this.RemoveFilterToolStripMenuItem_Click);
+            // 
+            // selectAllToolStripMenuItem
+            // 
+            this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+            this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(228, 24);
+            this.selectAllToolStripMenuItem.Text = "Выделить все";
+            this.selectAllToolStripMenuItem.Click += new System.EventHandler(this.SelectAllToolStripMenuItem_Click);
+            // 
+            // comboBoxSort
+            // 
+            this.comboBoxSort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSort.FormattingEnabled = true;
+            this.comboBoxSort.Items.AddRange(new object[] {
+            "По названию (А-Я)",
+            "По названию (Я-А)",
+            "По автору (А-Я)",
+            "По автору (Я-А)",
+            "По ID (новые сначала)",
+            "По ID (старые сначала)",
+            "Выбрать все"});
+            this.comboBoxSort.Location = new System.Drawing.Point(748, 26);
+            this.comboBoxSort.Name = "comboBoxSort";
+            this.comboBoxSort.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxSort.TabIndex = 10;
+            this.comboBoxSort.SelectedIndexChanged += new System.EventHandler(this.ComboBoxSort_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(745, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Сортировка";
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "ID";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.Width = 125;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Название книги";
+            this.Title.MinimumWidth = 6;
+            this.Title.Name = "Title";
+            this.Title.Width = 125;
+            // 
+            // Author
+            // 
+            this.Author.HeaderText = "Автор книги";
+            this.Author.MinimumWidth = 6;
+            this.Author.Name = "Author";
+            this.Author.Width = 125;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(982, 479);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxSort);
+            this.Controls.Add(this.dataGridViewBooks);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtTitle);
@@ -118,9 +234,10 @@
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnGroup);
-            this.Controls.Add(this.listBoxBooks);
             this.Name = "Form1";
             this.Text = "Управление книгами";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
+            this.contextMenuSortFilter.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -128,6 +245,18 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewBooks;
+        private System.Windows.Forms.ContextMenuStrip contextMenuSortFilter;
+        private System.Windows.Forms.ToolStripMenuItem sortAToZToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortZToAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem removeFilterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBoxSort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Author;
         //#region Код, автоматически созданный конструктором форм Windows
         //
         ///// <summary>
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,34 +16,60 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private Logic logic = new Logic();
-
+        private readonly Logic logic = new Logic();
         public Form1()
         {
             InitializeComponent();
-
-            var testRepo = new EntityRepository<Book>();
-
-            RefreshList();
+            SetupDataGridView();
+            RefreshDataGrid();
         }
 
-        /// <summary>   
-        /// Обновляет содержимое списка книг
-        /// </summary
-        private void RefreshList()
+        /// <summary>
+        /// Настройка DataGridView
+        /// </summary>
+        private void SetupDataGridView()
         {
-            listBoxBooks.Items.Clear();
-            foreach (var book in logic.GetAll())
-                listBoxBooks.Items.Add($"{book.Id}: {book.Title} - {book.Author}");
+            // FullRowSelect - при клике выделяется вся строка, а не отдельная ячейка
+            dataGridViewBooks.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            // можно выбрать несколько строк сразу с ctrl или shift
+            dataGridViewBooks.MultiSelect = true;
+            // Запрещаем пользователю редактировать ячейки напрямую в таблице
+            dataGridViewBooks.ReadOnly = true;
+            // Запрещаем отображение специальной строки для добавления новых записей
+            dataGridViewBooks.AllowUserToAddRows = false;
+            // Убирает колонку слева от строк таблицы
+            dataGridViewBooks.RowHeadersVisible = false;
+            // Автоматически растягиваем колонки чтобы заполнить всю доступную ширину
+            dataGridViewBooks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Привязка контекстного меню
+            // Указываем, что при правом клике по таблице должно показываться contextMenuSortFilter
+            dataGridViewBooks.ContextMenuStrip = contextMenuSortFilter;
         }
+        /// <summary>   
+        /// Обновляет содержимое таблицы книг
+        /// </summary>
+        private void RefreshDataGrid(IEnumerable<Book> books = null)
+        {
+            // Удаляем все элементы
+            dataGridViewBooks.Rows.Clear();
+
+            var data = books ?? logic.GetAll();
+
+            foreach (var book in data)
+            {
+                dataGridViewBooks.Rows.Add(book.Id, book.Title, book.Author);
+            }
+        }
+
         /// <summary>
         /// Обработчик кнопки добавления новой книги
         /// </summary>
-        private void btnAdd_Click(object sender, EventArgs e)
+        private void BtnAdd_Click(object sender, EventArgs e)
         {
-            if(logic.Add(txtTitle.Text, txtAuthor.Text))
+            if (logic.Add(txtTitle.Text, txtAuthor.Text))
             {
-                RefreshList();
+                RefreshDataGrid();
                 txtAuthor.Text = null;
                 txtTitle.Text = null;
             }
@@ -51,46 +77,156 @@ namespace WindowsFormsApp1
             {
                 MessageBox.Show("Заполните поле название и поле автор!");
             }
-                
-
         }
+
         /// <summary>
         /// Обработчик кнопки удаления выбранной книги
         /// </summary>
-        private void btnDelete_Click(object sender, EventArgs e)
+        private void BtnDelete_Click(object sender, EventArgs e)
         {
-            if (listBoxBooks.SelectedItem == null)
+            // Проверяет выбрана ли вообще книга
+            if (dataGridViewBooks.SelectedRows.Count == 0)
             {
                 MessageBox.Show("Выберите книгу для удаления.");
                 return;
             }
 
-            // Пример строки: "3: Название - Автор"
-            string selected = listBoxBooks.SelectedItem.ToString();
-            int id = int.Parse(selected.Split(':')[0]); // Получаем ID
+            // Удаляем выбранные строки
+            foreach (DataGridViewRow row in dataGridViewBooks.SelectedRows)
+            {
+                int id = (int)row.Cells["Id"].Value;
+                logic.Delete(id);
+            }
 
-            logic.Delete(id); // Удаляем по ID
-            RefreshList();    // Обновляем список
+            RefreshDataGrid();
         }
+
         /// <summary>
         /// Обработчик кнопки обновления данных книги
         /// </summary>
-        private void btnUpdate_Click(object sender, EventArgs e)
+        private void BtnUpdate_Click(object sender, EventArgs e)
         {
             foreach (var book in logic.GetAll())
             {
                 logic.Update(book.Id, book.Title, book.Author);
             }
-            RefreshList();
+            RefreshDataGrid();
         }
+
         /// <summary>
         /// Обработчик кнопки группировки книг по авторам
-        /// </summary
-        private void btnGroup_Click(object sender, EventArgs e)
+        /// </summary>
+        private void BtnGroup_Click(object sender, EventArgs e)
         {
-            listBoxBooks.Items.Clear();
-            foreach (var group in logic.GroupByAuthor())
-                listBoxBooks.Items.Add(group);
+            // Получаем все книги из базы данных и группируем их по автору
+            var groupedBooks = logic.GetAll()
+                .GroupBy(b => b.Author)         // Группируем книги по полю Author
+                .OrderBy(g => g.Key);           // Сортируем группы по имени автора (А-Я)
+
+            // Очищаем все существующие строки в DataGridView перед добавлением новых данных
+            dataGridViewBooks.Rows.Clear();
+
+            // groupedBooks содержит коллекцию групп, где каждая группа - книги одного автора
+            foreach (var group in groupedBooks)
+            {
+                // group.Key - имя автора
+                // group - коллекция книг этого автора
+
+                // Добавляем новую строку в таблицу для заголовка группы
+                int rowIndex = dataGridViewBooks.Rows.Add();
+                // Заполняем ячейку "Название книги" заголовком группы
+                dataGridViewBooks.Rows[rowIndex].Cells["Title"].Value = $"{group.Key}";
+                // Серый фон для визуального выделения заголовка
+                dataGridViewBooks.Rows[rowIndex].DefaultCellStyle.BackColor = Color.LightGray;
+                // Устанавливаем жирный шрифт для заголовка группы
+                dataGridViewBooks.Rows[rowIndex].DefaultCellStyle.Font = new Font(dataGridViewBooks.Font, FontStyle.Bold);
+
+                // Добавляем книги этой группы
+                foreach (var book in group)
+                {
+                    dataGridViewBooks.Rows.Add(book.Id, book.Title, book.Author);
+                }
+            }
+        }
+
+        // Обработчики контекстного меню
+        // Сортировка по возрастаню
+        private void SortAToZToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var books = logic.GetAll().OrderBy(b => b.Title).ToList();
+            RefreshDataGrid(books);
+        }
+        // Сортировка по убыванию
+        private void SortZToAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var books = logic.GetAll().OrderByDescending(b => b.Title).ToList();
+            RefreshDataGrid(books);
+        }
+        // Сброс сортировки
+        private void RemoveFilterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RefreshDataGrid();
+        }
+        // Выбрать все
+        private void SelectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridViewBooks.SelectAll();
+        }
+        // sortBy это поле для сортировки: "title", "author" или "id"
+        // direction это направление сортировки Ascending возрастание и Descending убывание
+        private void SortBooks(string sortBy, ListSortDirection direction)
+        {
+            // IEnumerable<Book> - это интерфейс, который позволяет перебирать элементы коллекции
+            IEnumerable<Book> sortedBooks;
+
+            // ToLower() на всякий случай
+            switch (sortBy.ToLower())
+            {
+                // сортировка по названию
+                case "title":
+                    // Если direction = Ascending, сортируем по возрастанию, иначе - по убыванию
+                    sortedBooks = direction == ListSortDirection.Ascending
+                        ? logic.GetAll().OrderBy(b => b.Title)
+                        : logic.GetAll().OrderByDescending(b => b.Title);
+                    break;
+                 // сортировка по автору
+                case "author":
+                    sortedBooks = direction == ListSortDirection.Ascending
+                        ? logic.GetAll().OrderBy(b => b.Author)
+                        : logic.GetAll().OrderByDescending(b => b.Author);
+                    break;
+                // сортировка по id
+                case "id":
+                    sortedBooks = direction == ListSortDirection.Ascending
+                        ? logic.GetAll().OrderBy(b => b.Id)
+                        : logic.GetAll().OrderByDescending(b => b.Id);
+                    break;
+
+                default:
+                    sortedBooks = logic.GetAll();
+                    break;
+            }
+            // Вызываем метод обновления таблицы, передавая отсортированный список, в List<Book> для работы с коллекцией
+            RefreshDataGrid(sortedBooks.ToList());
+        }
+        // Обработчик события изменения выбранного элемента в выпадающем списке сортировки
+        // Вызывается автоматически при выборе пользователем любого пункта в ComboBox
+        private void ComboBoxSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Проверяем, что в ComboBox действительно выбран какой-то элемент
+            // SelectedIndex = -1 означает, что ничего не выбрано
+            if (comboBoxSort.SelectedIndex == -1) return;
+            // SelectedIndex - числовой индекс выбранного элемента
+            switch (comboBoxSort.SelectedIndex)
+            {
+                case 0: SortBooks("title", ListSortDirection.Ascending); break;
+                case 1: SortBooks("title", ListSortDirection.Descending); break;
+                case 2: SortBooks("author", ListSortDirection.Ascending); break;
+                case 3: SortBooks("author", ListSortDirection.Descending); break;
+                case 4: SortBooks("id", ListSortDirection.Descending); break;
+                case 5: SortBooks("id", ListSortDirection.Ascending); break;
+                case 6: dataGridViewBooks.SelectAll(); break;
+            }
         }
     }
 }
